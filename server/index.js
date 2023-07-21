af83db6a012fee7877a3b30281dbb256df01236d
Awadhoot <awadhootk6@gmail.com>
2023-07-21 06:08:17 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+import postRouter from './routes/posts.js'
+
+const app = express()
+
+app.use('/posts', postRouter)
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+const CONNECTION_URL =
+  'mongodb+srv://awadhootk6:jnpppllfb83@cluster0.rxf3cul.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 3000
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  })
+  .catch(e => console.log(e))
